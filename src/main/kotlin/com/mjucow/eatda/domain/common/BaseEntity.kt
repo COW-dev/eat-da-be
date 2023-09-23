b5f52aa78c09e29528cfc3d67d19e0837dcc4166
doji <doji@channel.io>
2023-09-23 22:40:38 +0900

    불필요한 import 삭제

--- src/main/kotlin/com/mjucow/eatda/domain/common/BaseEntity.kt
+++ src/main/kotlin/com/mjucow/eatda/domain/common/BaseEntity.kt
@@ -1,8 +1,6 @@
 package com.mjucow.eatda.domain.common
 
 import jakarta.persistence.*
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 import java.time.ZoneId
