7918be059460c76fa988f0c44305e63c0e9f8570
Bokyeom <79684339+k-kbk@users.noreply.github.com>
2023-09-23 23:38:32 +0900

    [feature] 공통 예외처리 컨트롤러 구현 (#11)
    * ApiResponse 구현
    
    * GlobalExceptionHandler 추가
    
    * ControllerAdvice 어노테이션 구현
    
    * ktlint
    
    * ktlint

---
+++ src/main/kotlin/com/mjucow/eatda/presentation/common/GlobalExceptionHandler.kt
@@ -0,0 +1,27 @@
+package com.mjucow.eatda.presentation.common
+
+import jakarta.persistence.EntityNotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@GlobalControllerAdvice
+class GlobalExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgumentException(exception: IllegalArgumentException): ApiResponse<Unit> {
+        return ApiResponse.error(exception.message)
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(EntityNotFoundException::class)
+    fun handleEntityNotFoundException(exception: EntityNotFoundException): ApiResponse<Unit> {
+        return ApiResponse.error(exception.message)
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(RuntimeException::class)
+    fun handleRuntimeException(exception: RuntimeException): ApiResponse<Unit> {
+        return ApiResponse.error(exception.message)
+    }
+}
