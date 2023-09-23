a46408634c7e38920310bbe0e4e82c06c291392d
k-kbk <kkbk0077@gmail.com>
2023-09-23 22:57:15 +0900

    ApiResponse 구현

---
+++ src/main/kotlin/com/mjucow/eatda/presentation/common/ApiResponse.kt
@@ -0,0 +1,12 @@
+package com.mjucow.eatda.presentation.common
+
+data class ApiResponse<T>(
+    val message: String? = null,
+    val body: T? = null
+) {
+    companion object {
+        fun error(message: String?): ApiResponse<Unit> = ApiResponse(message = message)
+
+        fun <T> success(body: T?): ApiResponse<T> = ApiResponse(body = body)
+    }
+}
