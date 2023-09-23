cc345230c3403a68cb0566aafcd93bc469c6bc0a
doji <doji@channel.io>
2023-09-23 18:44:35 +0900

    setup ktlint gradle and github action

--- src/test/kotlin/com/mjucow/eatda/EatdaApplicationTests.kt
+++ src/test/kotlin/com/mjucow/eatda/EatdaApplicationTests.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.junit.jupiter.Testcontainers
 
-
 @SpringBootTest
 @Testcontainers
 @ActiveProfiles("test")
