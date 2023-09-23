0614a9a087d5e194b3e1419fcb1fb15ef4be2f44
k-kbk <kkbk0077@gmail.com>
2023-09-23 23:36:00 +0900

    ktlint

--- src/main/kotlin/com/mjucow/eatda/presentation/common/ApiResponse.kt
+++ src/main/kotlin/com/mjucow/eatda/presentation/common/ApiResponse.kt
@@ -2,7 +2,7 @@ package com.mjucow.eatda.presentation.common
 
 data class ApiResponse<T>(
     val message: String? = null,
-    val body: T? = null
+    val body: T? = null,
 ) {
     companion object {
         fun error(message: String?): ApiResponse<Unit> = ApiResponse(message = message)
