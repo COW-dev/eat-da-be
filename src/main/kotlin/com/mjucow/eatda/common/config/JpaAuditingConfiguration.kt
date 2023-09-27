ed7e91095d1b06ed64f1c3e8354c03bc8f16758f
유도진 <djyou128@gmail.com>
2023-09-27 16:15:32 +0900

    [etc] restdocs와 openapi3를 이용한 swagger 생성 (#15)

---
+++ src/main/kotlin/com/mjucow/eatda/common/config/JpaAuditingConfiguration.kt
@@ -0,0 +1,8 @@
+package com.mjucow.eatda.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaAuditingConfiguration
