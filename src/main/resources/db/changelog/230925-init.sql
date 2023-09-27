c43c5acbe5ba38324ac23c1350fc4c76326f8a2e
유도진 <djyou128@gmail.com>
2023-09-28 00:10:31 +0900

    [etc] sycn sql & update final id (#17)

--- src/main/resources/db/changelog/230925-init.sql
+++ src/main/resources/db/changelog/230925-init.sql
@@ -2,10 +2,10 @@
 
 -- changeset liquibase:1
 CREATE TABLE Category (
-    id          bigint      NOT NULL,
-    name        varchar(31) NOT NULL,
-    created_at  timestamp   NOT NULL,
-    updated_at  timestamp   NOT NULL
+    id          bigint                      NOT NULL,
+    name        varchar(31)                 NOT NULL,
+    created_at  timestamp with time zone    NOT NULL,
+    updated_at  timestamp with time zone    NOT NULL
 );
 
 ALTER TABLE Category ADD CONSTRAINT PK_CATEGORY PRIMARY KEY (id);
