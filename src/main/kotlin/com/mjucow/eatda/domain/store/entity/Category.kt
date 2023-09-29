28e70b9d1a9583c0f6d9189b3de487a54fa3fb15
doji <doji@channel.io>
2023-09-29 22:33:49 +0900

    category쪽 store 정보 제거

--- src/main/kotlin/com/mjucow/eatda/domain/store/entity/Category.kt
+++ src/main/kotlin/com/mjucow/eatda/domain/store/entity/Category.kt
@@ -1,13 +1,8 @@
 package com.mjucow.eatda.domain.store.entity
 
 import com.mjucow.eatda.domain.common.BaseEntity
-import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.JoinTable
-import jakarta.persistence.ManyToMany
 import jakarta.persistence.Table
 
 @Entity
@@ -24,18 +19,6 @@ class Category() : BaseEntity() {
             validateName(value)
             field = value
         }
-
-    @ManyToMany(fetch = FetchType.LAZY, cascade = [CascadeType.PERSIST, CascadeType.MERGE])
-    @JoinTable(
-        name = "store_category",
-        joinColumns = [JoinColumn(name = "category_id")],
-        inverseJoinColumns = [JoinColumn(name = "store_id")]
-    )
-    protected val mutableStores: MutableSet<Store> = mutableSetOf()
-
-    fun getStores(): Set<Store> {
-        return mutableStores.toSet()
-    }
 
     private fun validateName(name: String) {
         require(name.isNotBlank() && name.trim().length <= MAX_NAME_LENGTH)
