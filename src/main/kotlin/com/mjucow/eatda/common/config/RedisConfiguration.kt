5885455b0c91ed0db73d7281165e1d31a51cb985
doji <doji@channel.io>
2023-11-19 17:21:09 +0900

    [etc] add string redis template

--- src/main/kotlin/com/mjucow/eatda/common/config/RedisConfiguration.kt
+++ src/main/kotlin/com/mjucow/eatda/common/config/RedisConfiguration.kt
@@ -7,24 +7,36 @@ import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.serializer.StringRedisSerializer
+import org.springframework.data.redis.core.StringRedisTemplate
 
 @Configuration
 class RedisConfiguration(
     private val redisProperties: RedisProperties
 ) {
+    init {
+        println(redisProperties)
+    }
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
+        println("redisConnectionFactory")
         return LettuceConnectionFactory(
             RedisStandaloneConfiguration(redisProperties.host, redisProperties.port)
         )
     }
 
     @Bean
+    fun stringRedisTemplate(redisConnectionFactory: RedisConnectionFactory): StringRedisTemplate {
+        println("StringRedisTemplate")
+        return StringRedisTemplate().apply {
+            connectionFactory = redisConnectionFactory
+        }
+    }
+
+    @Bean
     fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
+        println("redisTemplate")
         return RedisTemplate<String, Any>().apply {
             connectionFactory = redisConnectionFactory
-            setDefaultSerializer(StringRedisSerializer())
         }
     }
 }
