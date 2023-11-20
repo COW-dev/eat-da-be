a7159194b33d7b6cf8f8613eb6ca53e4a70ad635
doji <doji@channel.io>
2023-11-21 01:27:07 +0900

    [enhance] remove postgresql test container

--- src/test/kotlin/com/mjucow/eatda/domain/AbstractDataTest.kt
+++ src/test/kotlin/com/mjucow/eatda/domain/AbstractDataTest.kt
@@ -4,11 +4,8 @@ import com.mjucow.eatda.common.config.JacksonConfiguration
 import com.mjucow.eatda.common.config.JooqContextConfiguration
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Import(value = [JooqContextConfiguration::class, JacksonConfiguration::class])
@@ -17,12 +14,4 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 abstract class AbstractDataTest {
-    companion object {
-        @Container
-        @ServiceConnection
-        val POSTGRESQL_CONTAINER = PostgreSQLContainer("postgres:15.4-alpine")
-            .withUsername("test-user")
-            .withPassword("test-password")
-            .withDatabaseName("test_db")!!
-    }
 }
