6b6d7f136c9734cdfe422c25e6e6e2e3c9088e16
doji <doji@channel.io>
2023-11-26 17:13:11 +0900

    add package

--- src/main/kotlin/com/mjucow/eatda/common/config/WebConfig.kt
+++ src/main/kotlin/com/mjucow/eatda/common/config/WebConfig.kt
@@ -1,3 +1,5 @@
+package com.mjucow.eatda.common.config
+
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
