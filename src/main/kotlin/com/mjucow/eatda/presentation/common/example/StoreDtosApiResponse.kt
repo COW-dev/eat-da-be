8890c082a7f634ebff3bb0b32175b46514098dcb
Gopistol <rhrjs0131@naver.com>
2024-01-03 23:49:42 +0900

    리뷰 반영해서 수정

--- src/main/kotlin/com/mjucow/eatda/presentation/common/example/StoreDtosApiResponse.kt
+++ src/main/kotlin/com/mjucow/eatda/presentation/common/example/StoreDtosApiResponse.kt
@@ -4,11 +4,7 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 
-@Target(
-    AnnotationTarget.ANNOTATION_CLASS,
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.CLASS
-)
+@Target(AnnotationTarget.FUNCTION)
 @ApiResponse(
     responseCode = "200",
     description = "성공",
