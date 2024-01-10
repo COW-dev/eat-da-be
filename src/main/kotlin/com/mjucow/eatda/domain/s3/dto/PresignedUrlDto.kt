582e5e11f28c42b6d5ea57a0ed9cc0af186c8985
k-kbk <kkbk0077@gmail.com>
2024-01-10 18:41:51 +0900

    lint

--- src/main/kotlin/com/mjucow/eatda/domain/s3/dto/PresignedUrlDto.kt
+++ src/main/kotlin/com/mjucow/eatda/domain/s3/dto/PresignedUrlDto.kt
@@ -3,7 +3,7 @@ package com.mjucow.eatda.domain.s3.dto
 data class PresignedUrlDto(
     val url: String,
 ) {
-    
+
     companion object {
         fun from(url: String): PresignedUrlDto {
             return PresignedUrlDto(url)
