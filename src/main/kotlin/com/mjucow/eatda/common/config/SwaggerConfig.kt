65a1b140b4c6965d653a52ee8a7992faa71f25aa
doji <doji@channel.io>
2024-03-03 23:51:19 +0900

    fix swagger url

--- src/main/kotlin/com/mjucow/eatda/common/config/SwaggerConfig.kt
+++ src/main/kotlin/com/mjucow/eatda/common/config/SwaggerConfig.kt
@@ -3,14 +3,20 @@ package com.mjucow.eatda.common.config
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class SwaggerConfig {
+class SwaggerConfig(
+    @Value("\${swagger.api-url}")
+    val apiUrl: String,
+) {
     @Bean
     fun swaggerApi(): OpenAPI = OpenAPI()
         .components(Components())
+        .servers(listOf(Server().apply { url = apiUrl }))
         .info(
             Info()
                 .title("Eatda API Documentation")
