e96269e5ba3897de0a6f297a38a8102b613c27da
doji <doji@channel.io>
2024-03-03 22:43:22 +0900

    add

---
+++ src/main/kotlin/com/mjucow/eatda/health/HealthCheckController.kt
@@ -0,0 +1,19 @@
+package com.mjucow.eatda.health
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health-check")
+class HealthCheckController {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    fun healthCheck(): String {
+        return "ok"
+    }
+
+}
